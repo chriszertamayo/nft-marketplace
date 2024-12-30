91d1736b4e31f775e739acef1b45efad88c762e7
chriszer tamayo <chriszertamayo@chriszers-MacBook-Pro.local>
2024-12-30 23:57:58 +0800

    add useNetwork hook

--- next_client/app/components/hooks/web3/useNetwork.ts
+++ next_client/app/components/hooks/web3/useNetwork.ts
@@ -1,8 +1,23 @@
 import { CryptoHookFactory } from "@/app/types/hooks"
 import useSWR from "swr"
 
+const NETWORKS: { [k: string]: string } = {
+    1: "Ethereum Main Network",
+    3: "Ropsten Test Network",
+    4: "RInkeby Test Network",
+    5: "Goerli Test Network",
+    42: "Kovan Test Network",
+    56: "Binance Smart Chain",
+    1337: "Ganache",
+}
+
+const targetId = <string>process.env.NEXT_PUBLIC_TARGET_CHAIN_ID
+const targetNetwork = NETWORKS[targetId]
+
 type UseNetworkResponse = {
     isLoading: boolean
+    isSupported: boolean
+    targetNetwork: string
 }
 
 type NetworkHookFactory = CryptoHookFactory<any, UseNetworkResponse>
@@ -16,7 +31,13 @@ export const hookFactory: NetworkHookFactory =
         const { data, isValidating, ...swr } = useSWR(
             provider ? "web3/useNetwork" : null,
             async () => {
-                return "Testing network"
+                const chaindId = parseInt((await provider!.getNetwork()).chainId.toString())
+
+                if (!chaindId) {
+                    throw "Cannot retreive network. Please refresh the browser."
+                }
+
+                return NETWORKS[chaindId]
             },
             {
                 revalidateOnFocus: false,
@@ -27,6 +48,8 @@ export const hookFactory: NetworkHookFactory =
             ...swr,
             data,
             isValidating,
+            targetNetwork,
+            isSupported: data === targetNetwork,
             isLoading: isLoading || isValidating,
         }
     }
